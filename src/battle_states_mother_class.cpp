790cfcb42cdd09a45ca856539a9d85de4be86541
Etienne Bertin <etienne.bertin@ensta-paristech.fr>
2024-12-03 21:15:23 +0100

    to string function displays when one side wins

--- src/battle_states_mother_class.cpp
+++ src/battle_states_mother_class.cpp
@@ -10,8 +10,21 @@ string BattleStates::toString () {
     stringstream output;
 
     // Iterate through each vector in the outer vector
+    int attacker_win_it = 0;
+    int defender_win_it = 0;
     for (size_t i = 0; i < _dice_rolls.size(); ++i) {
-        output << "state " << to_string(i) << " rolls:";
+        output << "state " << to_string(i);
+        if (i==_states_where_attacker_wins[attacker_win_it]) {
+            attacker_win_it++;
+            output << " attacker wins\n";
+            continue;
+        }
+        if (i==_states_where_defender_wins[defender_win_it]) {
+            defender_win_it++;
+            output << " defender wins\n";
+            continue;
+        }
+        output << " rolls:";
         // Iterate through each element in the inner vector
         for (size_t j = 0; j < _dice_rolls[i].size(); ++j) {
             output << " chance " << to_string(get<0>(_dice_rolls[i][j])) << " to state ";
