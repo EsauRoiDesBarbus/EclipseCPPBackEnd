947ea7e725ca829ba0d5ce6285067c221bc9596e
Etienne Bertin <etienne.bertin@ensta-paristech.fr>
2024-12-04 17:21:45 +0100

    removed warnings from tostring

--- src/battle_states_mother_class.cpp
+++ src/battle_states_mother_class.cpp
@@ -9,10 +9,12 @@ string BattleStates::toString () {
     // returns the entire battle as a string
     stringstream output;
 
+    int nb_states = _dice_rolls.size();
+
     // Iterate through each vector in the outer vector
     int attacker_win_it = 0;
     int defender_win_it = 0;
-    for (size_t i = 0; i < _dice_rolls.size(); ++i) {
+    for (int i = 0; i < nb_states; ++i) {
         output << "state " << to_string(i);
         if (i==_states_where_attacker_wins[attacker_win_it]) {
             attacker_win_it++;
@@ -26,20 +28,22 @@ string BattleStates::toString () {
         }
         output << " rolls:";
         // Iterate through each element in the inner vector
-        for (size_t j = 0; j < _dice_rolls[i].size(); ++j) {
+        int nb_rolls = _dice_rolls[i].size();
+        for (int j = 0; j < nb_rolls; ++j) {
             output << " chance " << to_string(get<0>(_dice_rolls[i][j])) << " to state ";
-            for (size_t k = 0; k < get<1>(_dice_rolls[i][j]).size(); ++k) {
+            int nb_possible_states = get<1>(_dice_rolls[i][j]).size();
+            for (int k = 0; k < nb_possible_states; ++k) {
                 output << to_string(get<1>(_dice_rolls[i][j])[k]);
-                if (k < get<1>(_dice_rolls[i][j]).size() - 1) {
+                if (k < nb_possible_states - 1) {
                     output << ",";
                 }
             }
         
-            if (j < _dice_rolls[i].size() - 1) {
+            if (j < nb_rolls - 1) {
                 output << ";";
             }
         }
-        if (i < _dice_rolls.size() - 1) {
+        if (i < nb_states - 1) {
             output << "\n";
         }
     }
