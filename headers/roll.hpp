2abca742365d6cfc31109b8e8a009dee224e7585
Etienne Bertin <etienne.bertin@ensta-paristech.fr>
2024-12-07 17:30:56 +0100

    added RollUnallocated with Roll

--- headers/roll.hpp
+++ headers/roll.hpp
@@ -2,11 +2,30 @@
 #define ROLL_HPP
 
 #include <vector>
+#include <array>
 
 struct Roll {
-    // each roll is defined as a probability, and states it can lead to
+    // roll is defined as a probability, and states it can lead to
     float _proba;
     std::vector<int> _allocations;
 };
+
+struct Damage {
+    std::array <int, 4> _damage_faces; // number of *, number of **, number of ***, number of ****
+    
+    int& operator[](int i) {return _damage_faces[i];}
+};
+
+struct RollUnallocated {
+    // unallocated roll is defined as a probability, and the amount of damage to distribute
+    float _proba;
+    std::vector<Damage> _damages; //first vector is total hits, (second is partial hits (third is partial partial hits)...))
+    // full hits are hits that can be applied to all enemy ships, first partial hits are hits that can be applied to all ships except the one with the most shield
+    int _self_hits; //for rift canon TODO
+};
+
+
+
+
 
 #endif
