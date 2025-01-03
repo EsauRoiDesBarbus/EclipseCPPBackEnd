9c6e565374dc843480aabe5cdb63b0fd37d479fc
Etienne Bertin <etienne.bertin@ensta-paristech.fr>
2025-01-03 19:54:42 +0100

    added addModifier to eclipseCpp

--- pythonmodule/expose_to_python.cpp
+++ pythonmodule/expose_to_python.cpp
@@ -14,9 +14,6 @@
 using namespace boost::python;
 using namespace std;
 
-#define ATT 1
-#define DEF 0
-
 struct battleBuilder {
     // battle info
     vector<shared_ptr<Ship>> _attacker_ships;
@@ -29,14 +26,21 @@ struct battleBuilder {
     battleBuilder (): _attacker_ships(0), _defender_ships(0), _att({false, false}), _def({false, false}) {}
 
     string help () {
-        return "This is a helper class to enter battle info. Add ships, one at time, with:\naddShip (ATT/DEF, number, INT/CRU/DRE/SBA, initiative, hull, computer, shield, Weapons(y,o,b,r,p), Weapons(my,mo,mb,mr,mp)).\ny=number of yellow canons, o=orange canons... my=yellow missiles...\nThen, call solveBattle ()\n";
+        string output;
+        output += "This is a helper class to enter battle info. Add ships, one at time, with:\n";
+        output += "addShip (ATT/DEF, number, INT/CRU/DRE/SBA, initiative, hull, computer, shield, Weapons(y,o,b,r,p), Weapons(my,mo,mb,mr,mp))\n";
+        output += "ny=number of yellow canons, o=orange canons... my=yellow missiles...\n";
+        output += "Then, add modifiers (typically if one side follows NPC rules) with:\n";
+        output += "addModifier (ATT/DEF, NPC/ANTIMATTER_SPLITTER)\n";
+        output += "Finally, solve the battle with solveBattle ()\n";
+        return output;
     }
 
     string addShip (string side_string, int number, string type_string, int initiative, int hull, int computer, int shield, Weapons canons, Weapons missiles) {
         // check side
         int side;
-        if      (side_string=="ATT") side = ATT;
-        else if (side_string=="DEF") side = DEF;
+        if      (side_string=="ATT") side = ATTACKER;
+        else if (side_string=="DEF") side = DEFENDER;
         else return "side (1st arg) should be ATT or DEF. No ship added.";
 
         //check type
@@ -48,9 +52,29 @@ struct battleBuilder {
         else return "type (3rd arg) should be INT, CRU, DRE or SBA. No ship added.";
 
         //create ship
-        if (side==ATT) _attacker_ships.push_back (make_shared<Ship> (Ship(number, type, initiative, hull, computer, shield, canons, missiles)));
-        else           _defender_ships.push_back (make_shared<Ship> (Ship(number, type, initiative, hull, computer, shield, canons, missiles)));
+        if (side==ATTACKER) _attacker_ships.push_back (make_shared<Ship> (Ship(number, type, initiative, hull, computer, shield, canons, missiles)));
+        else                _defender_ships.push_back (make_shared<Ship> (Ship(number, type, initiative, hull, computer, shield, canons, missiles)));
 
+        return "OK";
+    }
+
+    string addModifier (string side_string, string modifier) {
+        // check side
+        int side;
+        if      (side_string=="ATT") side = ATTACKER;
+        else if (side_string=="DEF") side = DEFENDER;
+        else return "side (1st arg) should be ATT or DEF. No modifier added.";
+
+        // check modifier
+        if      (modifier=="NPC") {
+            if (side==ATTACKER) _att._is_npc = true;
+            else                _def._is_npc = true;
+        }
+        else if (modifier=="ANTIMATTER_SPLITTER") {
+            if (side==ATTACKER) _att._antimatter_splitter = true;
+            else                _def._antimatter_splitter = true;
+        }
+        else return "modifier (2nd arg) should be NPC or ANTIMATTER_SPLITTER. No modifier added.";
         return "OK";
     }
 
@@ -71,6 +95,7 @@ BOOST_PYTHON_MODULE(eclipseCpp) {
     class_<battleBuilder>("battleBuilder")
         .def("help"       , &battleBuilder::help)
         .def("addShip"    , &battleBuilder::addShip)
+        .def("addModifier", &battleBuilder::addModifier)
         .def("solveBattle", &battleBuilder::solveBattle)
     ;
     class_<Weapons>("Weapons", init<int, int, int, int, int>());
--- test.py
+++ test.py
@@ -4,7 +4,14 @@ from eclipseCpp import battleBuilder, Weapons
 test = battleBuilder()
 print (test.help())
 
-test.addShip("ATT", 2, "INT", 3, 0, 0, 0, Weapons(1,0,0,0,0), Weapons(0,0,0,0,0))
-test.addShip("DEF", 2, "INT", 3, 0, 0, 0, Weapons(1,0,0,0,0), Weapons(0,0,0,0,0))
+test.addShip("ATT", 1, "INT", 3, 1, 2, 0, Weapons(1,0,0,0,0), Weapons(0,0,0,0,0))
+test.addShip("ATT", 1, "CRU", 2, 1, 1, 0, Weapons(1,0,0,0,0), Weapons(0,0,0,0,0))
+test.addShip("DEF", 1, "CRU", 2, 1, 1, 0, Weapons(2,0,0,0,0), Weapons(0,0,0,0,0))
+
 
 print (test.solveBattle())
+
+test.addModifier ("DEF", "NPC")
+
+print (test.solveBattle())
+
