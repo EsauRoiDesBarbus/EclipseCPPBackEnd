c038b32bdddc6c172ce9d8c9ca53006c7d34981d
Etienne Bertin <etienne.bertin@ensta-paristech.fr>
2025-01-03 13:06:24 +0100

    added missiles to python mod

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-from eclipseCpp import battleBuilder
+from eclipseCpp import battleBuilder, Weapons
 
 
 test = battleBuilder()
 print (test.help())
 
-test.addShip("ATT", 2, "INT", 3, 0, 0, 0, 1, 0, 0, 0, 0)
-test.addShip("DEF", 2, "INT", 3, 0, 0, 0, 1, 0, 0, 0, 0)
+test.addShip("ATT", 2, "INT", 3, 0, 0, 0, Weapons(1,0,0,0,0), Weapons(0,0,0,0,0))
+test.addShip("DEF", 2, "INT", 3, 0, 0, 0, Weapons(1,0,0,0,0), Weapons(0,0,0,0,0))
 
 print (test.solveBattle())
